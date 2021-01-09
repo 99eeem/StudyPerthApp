30654023170b358037e71a7a128a8ae64a040ff5
99eeem <99eeem@gmail.com>
2021-01-09 11:09:59 +0900

    modified migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_233656) do
+ActiveRecord::Schema.define(version: 2021_01_06_043319) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
